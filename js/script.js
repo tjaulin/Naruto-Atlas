387d605d15a2435f8eab6e1a48f3b0ac6ff6a331
Thomas <thomas.jaulin@mcparq.be>
2024-02-09 16:28:23 +0100

    add style, navbar and some images

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 const baseurl = 'https://narutodb.xyz/api'
+const contentDiv = document.querySelector('.content');
 
 async function getAllCharacters() {
-    const characters = await fetch(`${baseurl}/character?limit=10000`)
-    const charactersJson = await characters.json();
-    console.log('charactersJson :')
-    console.log(charactersJson)
+    const charactersData = await fetch(`${baseurl}/character?limit=10000`)
+    const charactersJson = await charactersData.json();
+    const characters = charactersJson.characters;
+    console.log(characters);
+    contentDivHtml = ``;
+    for(i = 0; i < characters.length; i++) {
+        if(characters[i].name == "Naruto Uzumaki") {
+            console.log(characters[i]);
+        }
+        contentDivHtml += `
+            <div class="box">
+                <p>${characters[i].name}</p>
+                <img src="${characters[i].images[0]}" alt="Image of ${characters[i].name}"/>
+            </div>
+        `;
+    }
+    contentDiv.innerHTML = contentDivHtml;
 }
 
 getAllCharacters();
