6833d1a9c40b22ffd1326ca6b833eb7901e31f52
Thomas <thomas.jaulin@mcparq.be>
2024-02-12 17:29:21 +0100

    progress characters

--- js/script.js
+++ js/script.js
@@ -1,24 +1,54 @@
 const baseurl = 'https://narutodb.xyz/api'
-const contentDiv = document.querySelector('.content');
+const contentDiv = $('.content');
+const charactersButton = $('.characters-button');
+
+$('.characters-button').on('click', function(e) {
+    e.preventDefault();
+    console.log('charactersButton clicked');
+    launchLoader();
+    getAllCharacters();
+});
 
 async function getAllCharacters() {
     const charactersData = await fetch(`${baseurl}/character?limit=10000`)
     const charactersJson = await charactersData.json();
     const characters = charactersJson.characters;
-    console.log(characters);
+    launchLoader(true);
+
+    $('.main').addClass('d-flex justify-content-center align-items-center flex-wrap gap-3');
+
     contentDivHtml = ``;
     for(i = 0; i < characters.length; i++) {
         if(characters[i].name == "Naruto Uzumaki") {
             console.log(characters[i]);
         }
         contentDivHtml += `
-            <div class="box">
-                <p>${characters[i].name}</p>
-                <img src="${characters[i].images[0]}" alt="Image of ${characters[i].name}"/>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="${characters[i].images[0]}" alt="Image of ${characters[i].name}" width="250px" />
+                <div class="card-body">
+                    <h5 class="card-title">${characters[i].name}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
         `;
     }
-    contentDiv.innerHTML = contentDivHtml;
+    $('.main').html(contentDivHtml);
 }
 
-getAllCharacters();
+function launchLoader(stop = false) {
+    if(stop) {
+        $('.loader-container').remove();
+        return;
+    }
+    $('.title').text('Characters');
+    $('.main').html('');
+    $("body").append(`
+        <div class="loader-container position-absolute top-50 start-50 translate-middle">
+            <img src="./img/loader/naruto-loader.gif" alt="naruto loader">
+            <img src="./img/loader/sasuke-loader.gif" alt="sasuke loader">
+            <img src="./img/loader/sakura-loader.gif" alt="sakura loader">
+            <img src="./img/loader/kakashi-loader.gif" alt="kakashi loader">
+        </div>
+    `);
+}
